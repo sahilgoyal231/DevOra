9b15612bcbfffbe4f11726ad454a59e58c4a2631
Sahil <goyalsahil452@gmail.com>
2026-06-10 11:04:21 +0530

    Removed test files

--- test-appwrite.js
+++
@@ -1,6 +0,0 @@
-const { Client, Storage } = require("appwrite");
-const client = new Client();
-client.setEndpoint('https://cloud.appwrite.io/v1').setProject('YOUR_PROJECT_ID');
-const storage = new Storage(client);
-console.log("Positional:", storage.getFilePreview('bucketId', 'fileId'));
-console.log("Object:", storage.getFilePreview({ bucketId: 'bucketId', fileId: 'fileId' }));
--- test-appwrite3.js
+++
@@ -1,4 +0,0 @@
-const { Client, Storage } = require("appwrite");
-const storage = new Storage(new Client());
-console.log("Positional:", storage.getFilePreview('bucketId', 'fileId'));
-console.log("Object:", storage.getFilePreview({ bucketId: 'bucketId', fileId: 'fileId' }));
